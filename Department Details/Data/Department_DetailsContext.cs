27c01296a31260b743b2e936f9802677eea13a60
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-12 16:04:23 +0530

    Add project files.

---
+++ Department Details/Data/Department_DetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Department_Details.Models;
+
+namespace Department_Details.Data
+{
+    public class Department_DetailsContext : DbContext
+    {
+        public Department_DetailsContext (DbContextOptions<Department_DetailsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department_Details.Models.Department> Department { get; set; }
+    }
+}
---
+++ Department Details/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Department_Details.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string DeptName { get; set; }
+        public string Location { get; set; }
+        public int NumberOfStaff { get; set; }
+    }
+}
